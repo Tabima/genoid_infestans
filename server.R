fe5dfb97e01ef613cee2a830666a711c9f624630
Javier F. Tabima <caifaz01@gmail.com>
2013-08-23 09:53:51 -0500

    Added new colors

--- server.R
+++ server.R
@@ -58,7 +58,7 @@ shinyServer(function(input, output) {
       else{
        #Adding colors to the tip values according to the clonal lineage
        gen$other$tipcolor <<- pop(gen)
-       levels(gen$other$tipcolor) <<- c("blue","darkcyan","darkolivegreen","blue","yellow3","violet", "skyblue", "green", "blue", "orange", "orangered","magenta","cyan","darkorchid","darkred","aquamarine3","gold","tan","maroon", "red")
+       levels(gen$other$tipcolor) <<- c("blue","darkcyan","darkolivegreen","darkgoldenrod","red", rainbow(length(levels(gen$other$tipcolor)) - 4))
        gen$other$tipcolor <<- as.character(gen$other$tipcolor)
          #Running the tree, setting a cutoff of 50 and saving it into a variable to be plotted (a)
        if (input$tree=="nj"){
