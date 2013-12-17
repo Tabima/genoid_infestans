38c6e62e6071d659de22ed58cc95c60eb8700846
Javier F. Tabima <caifaz01@gmail.com>
2013-12-17 08:47:46 -0600

    Added replen to UPGMA

--- server.R
+++ server.R
@@ -70,7 +70,7 @@ output$distPlotTree <- renderPlot({
         a <<- phangorn::midpoint(ladderize(a))
        }
        else {
-         a <<- bruvo.boot(gen, sample = input$boot, tree = input$tree, cutoff = 50)
+         a <<- bruvo.boot(gen, replen = ssr, sample = input$boot, tree = input$tree, cutoff = 50)
        }
        #Drawing the tree
        plot.phylo(a)
